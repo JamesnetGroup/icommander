555492fa300b34cf2a98ea0f58a3d130f1babf2d
james.lee <james.lee@ncoresoft.net>
2021-02-09 14:04:09 +0900

    Update

--- Commander.LayoutSupport/Windows/Controls/ExplorerListBox.cs
+++ Commander.LayoutSupport/Windows/Controls/ExplorerListBox.cs
@@ -1,48 +1,50 @@
-﻿using Commander.Data.File;
-using Commander.WindowsBase.Windows.InputSupport;
-using Commander.WindowsReference.OperationSystem.Windows;
-using System.IO;
+﻿using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Commander.Data.File;
+using Commander.WindowsBase.Windows.InputSupport;
+using Commander.WindowsReference.OperationSystem.Windows;
 
 namespace Commander.Windows.Controls.Primitives
 {
     public class ExplorerListBox : ListBox
     {
-        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
-       "Command", typeof(ICommand), typeof(ExplorerListBox));
+        #region DependencyProperty
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register("Command", typeof(ICommand), typeof(ExplorerListBox));
+        #endregion
+
+        #region Command
+
         public ICommand Command
         {
             get { return (ICommand)this.GetValue(CommandProperty); }
             set { this.SetValue(CommandProperty, value); }
         }
+        #endregion
+
+        #region OnMouseRightButtonUp
 
         protected override void OnMouseRightButtonUp(MouseButtonEventArgs e)
         {
             base.OnMouseRightButtonUp(e);
 
-            if (e.OriginalSource is FrameworkElement fe && fe.DataContext is FileData data)
+            List<FileInfo> files;
+            ShellContextMenu ctx;
+
+            if (e.OriginalSource is FrameworkElement fe && fe.DataContext is FileData)
             {
-                FileData FileData = data;
+                files = new List<FileInfo>();
+                ctx = new ShellContextMenu();
 
-                ShellContextMenu ctxMnu = new ShellContextMenu();
-                FileInfo[] arrFI = new FileInfo[this.SelectedItems.Count];
-
-                int i = 0;
-                foreach (FileData file in this.SelectedItems)
-                {
-                    arrFI[i] = new FileInfo(file.FullName);
-                    i++;
-                }
-                if (arrFI.Length == 0)
-                {
-                    arrFI[0] = new FileInfo(FileData.FullName);
-                }
-                ctxMnu.ShowContextMenu(arrFI, MousePosition.GetMousePosition(this));
-
-                Command?.Execute(e);
+                // TODD James: Linq AddRange
+                files.AddRange(from FileData file in this.SelectedItems select new FileInfo(file.FullName));
+                ctx.ShowContextMenu(files.ToArray(), MousePosition.GetMousePosition(this));
             }
         }
+        #endregion
     }
 }
