174215fa9a5fae926c128fd0b6b2dc428e5aac30
vickyqu115 <vickyqu115@gmail.com>
2025-04-08 17:03:37 +0800

    Refactor ShellHelper class into its own file
    Moved ShellHelper class from ShellContextMenu.cs to ShellHelper.cs.
    
    Updated Commander.WindowsReference.csproj to include the new file.
    
    ShellHelper class contains HiWord and LoWord methods.
    
    Namespace remains Commander.WindowsReference.OperationSystem.Windows.

--- Commander.WindowsReference/OperationSystem/Windows/ShellContextMenu.cs
+++ Commander.WindowsReference/OperationSystem/Windows/ShellContextMenu.cs
@@ -1402,30 +1402,4 @@ namespace Commander.WindowsReference.OperationSystem.Windows
             int code, IntPtr wParam, IntPtr lParam);
         // ************************************************************************
     }
-
-    internal static class ShellHelper
-    {
-        /// <summary>
-        /// Retrieves the High Word of a WParam of a WindowMessage
-        /// </summary>
-        /// <param name="ptr">The pointer to the WParam</param>
-        /// <returns>The unsigned integer for the High Word</returns>
-        public static uint HiWord(IntPtr ptr)
-        {
-            if (((uint)ptr & 0x80000000) == 0x80000000)
-                return ((uint)ptr >> 16);
-            else
-                return ((uint)ptr >> 16) & 0xffff;
-        }
-
-        /// <summary>
-        /// Retrieves the Low Word of a WParam of a WindowMessage
-        /// </summary>
-        /// <param name="ptr">The pointer to the WParam</param>
-        /// <returns>The unsigned integer for the Low Word</returns>
-        public static uint LoWord(IntPtr ptr)
-        {
-            return (uint)ptr & 0xffff;
-        }
-    }
 }
